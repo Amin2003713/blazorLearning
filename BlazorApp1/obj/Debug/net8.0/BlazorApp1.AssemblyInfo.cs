aebb7484e2b7b90a659ad3976e4c00a11a46df61
amin1 <Amin1382422amin@gmail.com>
2024-08-20 12:18:22 +0330

    dft

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d8be1fb73e698a6b112460b94d91abc4eaeaa21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e23ce267bb4afd37d1018717b30b63b752049ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
